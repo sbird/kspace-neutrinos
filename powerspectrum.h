5b0c75f44ae436edb8d7d37fa65fc1288e492498
Simeon Bird <sbird@andrew.cmu.edu>
2016-01-06 15:41:13 -0500

    Remove now unused function, rebin_power

--- powerspectrum.h
+++ powerspectrum.h
@@ -8,7 +8,4 @@
  * Before use you may wish to normalise by dividing by count*count*/
 void total_powerspectrum(const int dims, fftw_complex *outfield, const int nrbins, const int startslab, const int nslab, double *power, long long int *count, double *keffs, const double total_mass);
 
-/*Rebin the power computed by total_powerspectrum by rebinning it to have even bins in logk. This reduces mode-by-mode dispersion*/
-void rebin_power(double logk[],double delta_cdm_curr[],const int nbins, double Kbin[], double SumPower[], long long CountModes[], const int bins_ps, const int pmgrid, const double BoxSize);
-
 #endif
