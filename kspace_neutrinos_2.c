c6bce1a4c0b99b9ba9c34eeda77383e76beaf346
Simeon Bird <sbird4@jhu.edu>
2016-12-09 09:53:07 -0500

    More comments

--- kspace_neutrinos_2.c
+++ kspace_neutrinos_2.c
@@ -172,6 +172,7 @@ void add_nu_power_to_rhogrid(const double Time, const double BoxSize, fftw_compl
   /* (binned) k values for the power spectrum*/
   double * keff = delta_cdm_curr+2*nk_allocated;
   long long int * count = mymalloc("temp_modecount", nk_allocated*sizeof(long long int));
+  const double scale=pow(2*M_PI/BoxSize,3);
   if(!delta_cdm_curr || !delta_nu_curr || !keff || !count)
       terminate("Could not allocate temporary memory for power spectra\n");
   /*We calculate the power spectrum at every timestep
@@ -179,7 +180,6 @@ void add_nu_power_to_rhogrid(const double Time, const double BoxSize, fftw_compl
    * This function stores the total power*no. modes.*/
   nk_in = total_powerspectrum(pmgrid, fft_of_rhogrid, nk_allocated, slabstart_y, nslab_y, delta_cdm_curr, count, keff, MYMPI_COMM_WORLD);
   /*Get delta_cdm_curr , which is P(k)^1/2, and convert P(k) to physical units. */
-  const double scale=pow(2*M_PI/BoxSize,3);
   for(i=0;i<nk_in;i++){
       delta_cdm_curr[i] = sqrt(delta_cdm_curr[i]/scale);
       keff[i] *= (2*M_PI/BoxSize);
--- powerspectrum.h
+++ powerspectrum.h
@@ -15,7 +15,19 @@
 #endif
 
 /* Compute the total powerspectrum from a Fourier-transformed density field in outfield, and store it in power.
- * Before use you may wish to normalise by dividing by count*count*/
+ * Before use you may wish to normalise by dividing by count^2 and 2*MPI/BoxSize. This assumes an FFTW2, slab decomposed, FFT.
+ * Arguments:
+ * dims - size of one dimension of the density grid.
+ * outfield - Fourier transformed density grid.
+ * nrbins - Number of bins allocated for the output power spectrum.
+ * slabstart - for slab parallelized FFT routines, this is the start index of the FFT on this rank.
+ * nslab - number of elements of the FFT on this rank.
+ * power - array for the output power spectrum. Returns power, in units of the box, UNNORMALISED.
+ * Only bins with > 0 modes are included.
+ * count - Number of modes in each bin.
+ * keffs - effective k, averaged over all modes in a bin
+ * MYMPI_COMM_WORLD - MPI communicator.
+ * Returns the number of bins in the output power spectrum, all of which have a non-zero number of modes.*/
 int total_powerspectrum(const int dims, fftw_complex *outfield, const int nrbins, const int startslab, const int nslab, double *power, long long int *count, double *keffs, const MPI_Comm MYMPI_COMM_WORLD);
 
 #endif
