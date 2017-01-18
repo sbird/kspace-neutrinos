ea499b95f0cd3bec22ed770099a7b9dcd336eca9
Simeon Bird <sbird4@jhu.edu>
2017-01-18 16:41:03 -0500

    Move redefinition of hubble function into the only test that uses it

--- delta_tot_table_test.c
+++ delta_tot_table_test.c
@@ -10,11 +10,40 @@
 #include "transfer_init.h"
 #include "omega_nu_single.h"
 #include "kspace_neutrino_const.h"
+#include "gadget_defines.h"
 
 #define  T_CMB0      2.7255	/* present-day CMB temperature, from Fixsen 2009 */
-/*Initialise the hubble function: this is defined in gadget_defines.c and forward declared here,
- *to avoid accidentally using it anywhere except in tests.*/
-void init_hubble_function(_omega_nu * omnu, const double Omega0, const double UnitTime_in_s);
+/* This is a bit goofy, because the hubble function in gadget uses global state.
+ * So in the tests we need to set the global state with init_hubble_function before using it,
+ * to keep the function definition the same.
+ * We COULD just use an internal version, but then we would break compatibility if anyone has
+ * used an odd hubble history for eg, some dark energy model. */
+static _omega_nu * m_omnu;
+static double Omega_nonu;
+static double OmegaLambda;
+static double m_Hubble;
+
+void init_hubble_function(_omega_nu * omnu, const double Omega0, const double UnitTime_in_s)
+{
+    m_omnu = omnu;
+    Omega_nonu = Omega0 - get_omega_nu(omnu, 1);
+    OmegaLambda = 1 - Omega0;
+    m_Hubble = HUBBLE * UnitTime_in_s;
+}
+
+double hubble_function(double a)
+{
+    if(!m_omnu) {
+        terminate("init_hubble_function was not called in test suite before this!\n");
+    }
+    /* Matter + Lambda: neglect curvature*/
+    double omega_tot = Omega_nonu/pow(a,3) + OmegaLambda;
+    /*Neutrinos*/
+    omega_tot += get_omega_nu(m_omnu, a);
+    /*Radiation*/
+    omega_tot += get_omegag(m_omnu, a);
+    return m_Hubble * sqrt(omega_tot);
+}
 
 /*A struct to hold some useful pointers*/
 struct _test_state {
