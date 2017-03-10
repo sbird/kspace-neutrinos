7177b7b7d3f0a32632e7a6fb97abe413dbf2117b
Simeon Bird <sbird4@jhu.edu>
2017-03-10 08:09:47 +0800

    Add short function to plot the linear neutrino power from CAMB

--- plot_nu_power.py
+++ plot_nu_power.py
@@ -1,5 +1,6 @@
 """Routine to plot the neutrino power spectrum, as output by this code."""
 
+import math
 import numpy as np
 
 def get_nu_power(filename):
@@ -12,3 +13,15 @@ def get_nu_power(filename):
     k = data[:,0]
     pnu = data[:,1]
     return (k, pnu)
+
+def get_camb_nu_power(matpow, transfer):
+    """Plot the neutrino power spectrum from CAMB.
+    This is just the matter power multiplied
+    by the neutrino transfer function.
+    CAMB internal units are used.
+    Assume they have the same k binning."""
+    matter = np.loadtxt(matpow)
+    trans = np.loadtxt(transfer)
+    #Adjust Fourier convention to match CAMB.
+    tnufac = (trans[:,5]/trans[:,6])**2
+    return matter[:,0], matter[:,1]*tnufac
